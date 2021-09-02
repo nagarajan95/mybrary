57da99d4366a1840b3424941dd0ab62198edefb9
= <=>
2021-09-03 02:49:19 +0530

    Initial Author Routes

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require("express")
 const app = express()
 const expressLayouts = require("express-ejs-layouts")
-
+const bodyParser = require('body-parser')
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 const mongoose = require('mongoose')
 
@@ -24,8 +25,9 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
-
+app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
 app.use('/', indexRouter)
+app.use('/authors',authorRouter)
 
 app.listen(process.env.PORT || 3000)
 
